22c866044b75c5485eb082b443d034ef60b2e633
parameterized <troyk212@gmail.com>
2017-07-10 21:29:46 -0400

    cmd to repeat test audio from file

--- index.js
+++ index.js
@@ -5,15 +5,17 @@ const client = new Discord.Client()
 
 const token = fs.readFileSync('./token.txt', 'utf8').trim();
 
+var voiceConnections = new Map();
 var voiceReceivers = new Map();
 var userStreams = new Map();
 var echo = false;
+var testSound = false;
 
 client.on('ready', () => {
 	console.log('ready');
 });
 
-client.on('message', message => {
+client.on('message', (message) => {
 	let mention = client.user.id + '>';
 	if (message.guild.me.nickname) {
 		mention = '<@!' + mention;
@@ -36,15 +38,35 @@ client.on('message', message => {
 					voiceReceivers.delete(message.member.voiceChannelID);
 				}
 				voiceConnection.disconnect();
+				voiceConnections.delete(message.member.voiceChannelID);
+				echo = false;
+				testSound = false;
 			} else {
-				message.member.voiceChannel.join().then(voiceConnection => {
+				message.member.voiceChannel.join().then((voiceConnection) => {
+					voiceConnections.set(message.member.voiceChannelID, voiceConnection);
+					voiceConnection.playFile('test/steam.mp3');
 					voiceReceivers.set(message.member.voiceChannelID, voiceConnection.createReceiver());
-
 				});
 			}
 		}
 	} else if (cmd == '.echo') {
 		echo = !echo;
+	} else if (cmd == '.test') {
+		let voiceConnection = message.guild.voiceConnection;
+		if (voiceConnection) {
+			testSound = !testSound;
+			let restartOrEnd = (message) => {
+				voiceConnection = voiceConnections.get(message.member.voiceChannelID);
+				if (testSound && voiceConnection && voiceConnection.status === 0) {
+					voiceConnection.playFile('test/PowerUp.wav').once('end', () => {
+						setTimeout(() => {
+							restartOrEnd(message);
+						}, 15);
+					});
+				}
+			};
+			restartOrEnd(message)
+		}
 	}
 });
 
@@ -58,10 +80,13 @@ function guildMemberSpeaking(member, speaking) {
 				userStreams.delete(member.id);
 			}
 			if (speaking) {
+				//doesn't work well
 				userStream = voiceReceiver.createOpusStream(member);
 				userStreams.set(member.id, userStream);
 				if (echo) {
-					voiceReceiver.voiceConnection.playOpusStream(userStream);
+					setTimeout(() => {
+						voiceReceiver.voiceConnection.playOpusStream(userStream);
+					}, 500);
 				}
 			}
 		}
