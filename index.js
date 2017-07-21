2f4d9551dbcfc31991c8637e875f1fee945411f8
parameterized <troyk212@gmail.com>
2017-07-21 00:26:27 -0400

    echo works, generating audio

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
 const fs = require('fs');
+const { Transform } = require('stream');
 const Discord = require('discord.js');
 const client = new Discord.Client()
 
@@ -8,8 +9,9 @@ const token = fs.readFileSync('./token.txt', 'utf8').trim();
 var voiceConnections = new Map();
 var voiceReceivers = new Map();
 var userStreams = new Map();
-var echo = false;
-var testSound = false;
+var echoOn = false;
+var testSoundOn = false;
+var testStreamOn = false;
 
 client.on('ready', () => {
 	console.log('ready');
@@ -39,8 +41,9 @@ client.on('message', (message) => {
 				}
 				voiceConnection.disconnect();
 				voiceConnections.delete(message.member.voiceChannelID);
-				echo = false;
-				testSound = false;
+				echoOn = false;
+				testSoundOn = false;
+				testStreamOn = false;
 			} else {
 				message.member.voiceChannel.join().then((voiceConnection) => {
 					voiceConnections.set(message.member.voiceChannelID, voiceConnection);
@@ -49,23 +52,59 @@ client.on('message', (message) => {
 				});
 			}
 		}
-	} else if (cmd == '.echo') {
-		echo = !echo;
-	} else if (cmd == '.test') {
+	} else if (cmd === '.echo') {
+		echoOn = !echoOn;
+	} else if (cmd === '.testfile') {
 		let voiceConnection = message.guild.voiceConnection;
 		if (voiceConnection) {
-			testSound = !testSound;
+			testSoundOn = !testSoundOn;
 			let restartOrEnd = (message) => {
 				voiceConnection = voiceConnections.get(message.member.voiceChannelID);
-				if (testSound && voiceConnection && voiceConnection.status === 0) {
+				if (testSoundOn && voiceConnection && voiceConnection.status === 0) {
 					voiceConnection.playFile('test/PowerUp.wav').once('end', () => {
 						setTimeout(() => {
 							restartOrEnd(message);
-						}, 15);
+						}, 1000);
 					});
 				}
 			};
 			restartOrEnd(message)
+		}
+	} else if (cmd === '.testpy') {
+		let voiceConnection = message.guild.voiceConnection;
+		if (voiceConnection) {
+			testStreamOn = !testStreamOn;
+			if (testStreamOn) {
+				let testStream = new Transform({
+					transform(chunk, encoding, callback) {
+						this.push(chunk);
+						callback();
+					}
+				});
+				let interval = setInterval(() => {
+					if (testStream) {
+						if (testStreamOn) {
+							let buf = Buffer.alloc(24000*4);
+							for (i=0; i < 24000; i++) {
+								for (j=0; j < 2; j++) {
+									buf.writeInt16LE(
+										Math.floor(
+											Math.sin(i/48000*6.2831*440)
+											*0.2*16383+16383
+										),
+										i*4+j*2
+									);
+								}
+							}
+							testStream.write(buf);
+						} else {
+							testStream.end();
+							clearInterval(interval);
+						}
+					}
+				}, 500);
+				voiceConnection.playConvertedStream(testStream);
+			}
 		}
 	}
 });
@@ -80,12 +119,11 @@ function guildMemberSpeaking(member, speaking) {
 				userStreams.delete(member.id);
 			}
 			if (speaking) {
-				//doesn't work well
-				userStream = voiceReceiver.createOpusStream(member);
+				userStream = voiceReceiver.createPCMStream(member);
 				userStreams.set(member.id, userStream);
-				if (echo) {
+				if (echoOn) {
 					setTimeout(() => {
-						voiceReceiver.voiceConnection.playOpusStream(userStream);
+						voiceReceiver.voiceConnection.playConvertedStream(userStream);
 					}, 500);
 				}
 			}
